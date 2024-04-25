db74d9bb37feaf6669cce200c29975da3fe6e41d
jackiecoughlin9 <jacqueline.coughlin@tufts.edu>
2024-04-25 13:33:30 -0400

    dkj

--- stock2.js
+++ stock2.js
@@ -41,10 +41,10 @@ http.createServer(function (req, res) {
         // Find matching documents
         const results = collection.find(query);
 
-        // Print a message if no documents were found
-        if ((await collection.countDocuments(query)) === 0) {
-            console.log("Stock not found");
-        }
+        // // Print a message if no documents were found
+        // if ((await collection.countDocuments(query)) == 0) {
+        //     console.log("Stock not found");
+        // }
 
         // Print document info
         // for await (const doc of results) {
