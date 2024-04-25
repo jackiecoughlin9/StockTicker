8f53a03b2a91320585ed5009298d2275c7f7f66a
jackiecoughlin9 <jacqueline.coughlin@tufts.edu>
2024-04-25 12:52:46 -0400

    onsd

--- stock2.js
+++ stock2.js
@@ -8,7 +8,6 @@ http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
     var queryObject = url.parse(req.url, true).query; 
     var pathName = url.parse(req.url, true).pathname;  
-    console.log("Nice"); 
 
     if (pathName == "/") {
       html = "<form method='GET' action='/process'>" +
@@ -30,6 +29,33 @@ http.createServer(function (req, res) {
     if (queryObject.radio == "name"){
       query = { "name": `${stock}`}; 
     }
+
+  //connect to mongo
+  const connStr = "mongodb+srv://jackiecoughlin99:Jackiec18@cluster0.mlvh003.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+  MongoClient.connect(connStr, async function(err, db) {   
+    if (err) { console.log(err); }
+    else {
+        // Select db and collection
+        var db_object = db.db("Stock");
+        var collection = db_object.collection('PublicCompanies');
+        // Find matching documents
+        const results = collection.find(query);
+
+        // Print a message if no documents were found
+        if ((await collection.countDocuments(query)) === 0) {
+            console.log("Stock not found");
+        }
+
+        // Print document info
+        for await (const doc of results) {
+            console.dir("Name: " + doc.name);
+            console.dir("Symbol: " + doc.symbol);
+            console.dir("Price: " + doc.price);
+        }
+        // Close database
+        await db.close();
+    } // end else
+}) // end client.connect
 
 
   res.end();
