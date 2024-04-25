3132945abe3cb7cf98da3880912d6d7cdd1d13c6
jackiecoughlin9 <167026902+jackiecoughlin9@users.noreply.github.com>
2024-04-25 00:02:34 -0400

    Create stock2.js

---
+++ stock2.js
@@ -0,0 +1,38 @@
+var http = require('http');
+var url = require('url'); 
+const MongoClient =  require('mongodb').MongoClient;
+// const port = process.env.PORT || 8080; 
+
+http.createServer(function (req, res) {
+  
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    var queryObject = url.parse(req.url, true).query; 
+    var pathName = url.parse(req.url, true).pathname;  
+
+    if (pathName == "/") {
+      html = "<form method='GET' action='/process'>" +
+      "<label for='stock'>Search by stock symbol or company name</label></br>" +
+      "<input type='text' id='stock' name='stock'></input></br>" + 
+      "<input type='radio' id='symbol' name='radio' value='symbol'></input>" + 
+      "<label for='symbol'>Symbol</label></br>" +
+      "<input type='radio' id='name' name='radio' value='name'></input>" +
+      "<label for='name'>Name</label></br>" + 
+      "<input type='submit' value='Search'>" +
+      "</form>";
+      res.write(html);
+  }
+  else if (pathName == "/process")
+    var stock = queryObject.stock;
+    if (queryObject.radio == "symbol") {
+        query = { "tickerSymbol": `${stock}`};
+        //read data 
+    } 
+    if (queryObject.radio == "name"){
+      query = { "name": `${stock}`}; 
+       //read data 
+    }
+
+
+  res.end();
+  
+}).listen(8080);
